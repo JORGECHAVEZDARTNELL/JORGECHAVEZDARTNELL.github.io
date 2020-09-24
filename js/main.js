5d5e89e2f1d0707db5e5a9a94958f2df5ad8a7fe
JORGECHAVEZDARTNELL <59939009+CodyDevelopers@users.noreply.github.com>
2020-09-24 15:29:00 -0500

    as

--- js/main.js
+++ js/main.js
@@ -2,19 +2,75 @@ const db = firebase.firestore();
 
 //login
 const loginForm = document.getElementById('login-form');
+var main = document.getElementById("main");
 
-const getLogin = (username, password) =>db.collection("users").where("username","==",username).where("password","==",password);
+//const getLogin = (username, password) =>db.collection("users").where("username","==",username).where("password","==",password).get();
+const getAula = db.collection("users").where("username","==",username.value).where("password","==",password.value).get();
+
 
 loginForm.addEventListener('submit', async(e)=>{
     e.preventDefault();
     const username = loginForm["login-username"];
     const password = loginForm["login-pass"];
-    const doc = await getLogin(username, password);
-    const user=doc.data();
-    console.log(user);
+    await db.collection("users").where("username","==",username.value).where("password","==",password.value).get()
+    .then(function(querySnapshot){
+        if(querySnapshot.size==1){
+            querySnapshot.forEach(function(doc){
+                loadNotas(doc);
+                loadAula();
+                
+            })
+        }
+    }).catch(function(error){
+        console.log("error");
+    });
+
+    //const user=doc.data();
+    //console.log(user);
 })
 
-/*
+async function loadContent(){
+    await db.collection("aulas").where("nivel","==", "1").where("grado","==","1")
+    .where("seccion","==","1").get()
+    .then(function(querySnapshot){
+        if(querySnapshot.size>=1){
+            querySnapshot.forEach(function(doc){
+                
+            })
+        }
+    }).catch(function(error){
+        console.log("error:2");
+    })
+}
+
+
+
+
+
+
+function loadNotas(doc){
+    const user = doc.data();
+    aulas= user.aulaseccion;
+    aulas.forEach(function(element){
+        console.log(element.split("-"));
+    });
+    main.innerHTML='';
+    main.innerHTML+=`  <section class="section section-xl bg-default">
+    <div class="container">
+        <div class="row">
+        <div class="col-lg-6">
+
+        </div>
+        </div>
+        <div class="row">
+        <div class="col-lg-6">
+            
+        </div>
+        </div>
+    </section>`;
+}
+
+
 
 //mesa de partes
 const tramiteForm = document.getElementById('tramite-form');
@@ -45,4 +101,4 @@ tramiteForm.addEventListener('submit', async(e)=>{
     taskForm.reset();
     windows.alert("HEHE");
 
-})*/
+})
