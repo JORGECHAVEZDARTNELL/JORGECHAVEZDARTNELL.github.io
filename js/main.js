d8a025058b298e8b81b01baae7eb234e2b30523f
JORGECHAVEZDARTNELL <59939009+CodyDevelopers@users.noreply.github.com>
2020-09-24 10:18:51 -0500

    as

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 const db = firebase.firestore();
 
+//login
+const loginForm = document.getElementById('login-form');
+
+const getLogin = (username, password) =>db.collection("users").where("username","==",username).where("password","==",password);
+
+loginForm.addEventListener('submit', async(e)=>{
+    e.preventDefault();
+    const username = loginForm["login-username"];
+    const password = loginForm["login-pass"];
+    const doc = await getLogin(username, password);
+    const user=doc.data();
+    console.log(user);
+})
+
+/*
+
+//mesa de partes
 const tramiteForm = document.getElementById('tramite-form');
-
-
 const saveTramite = (tipoTramite,asunto,correo,telefono,dni)=>{
     db.collection('tramites').doc().set({
         //title: title,
@@ -16,7 +31,6 @@ const saveTramite = (tipoTramite,asunto,correo,telefono,dni)=>{
 }
 //const getTask = ()=>db.collection('tasks').get();
 
-
 tramiteForm.addEventListener('submit', async(e)=>{
     e.preventDefault();
     const tipoTramite = tramiteForm["mesa-tipo-tramite"];
@@ -26,10 +40,9 @@ tramiteForm.addEventListener('submit', async(e)=>{
     const dni = tramiteForm["mesa-dni"];
     
     console.log(document.getElementById("mesa-doc").value);    
-    //console.log(title, description);
-    //await saveTramite(tipoTramite.value,asunto.value,correo.value,telefono.value,dni.value);
-    //taskForm.reset();
-    //windows.alert("HEHE");
-    
-    
-})
+    console.log(title, description);
+    await saveTramite(tipoTramite.value,asunto.value,correo.value,telefono.value,dni.value);
+    taskForm.reset();
+    windows.alert("HEHE");
+
+})*/
